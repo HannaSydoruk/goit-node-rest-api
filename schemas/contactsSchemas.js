a258607bd2774cbcd86a3a4283d028035fe0f46c
HannaSydoruk <hanna.sydoruk@gmail.com>
2024-03-05 14:31:47 +0200

    part1

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -3,15 +3,17 @@ import Joi from "joi";
 export const createContactSchema = Joi.object({
     name: Joi.string().required(),
     email: Joi.string().required(),
-    phone: Joi.string().required()
+    phone: Joi.string().required(),
+    favorite: Joi.boolean()
 })
 
 export const updateContactSchema = Joi.object({
     name: Joi.string(),
     email: Joi.string(),
-    phone: Joi.string()
+    phone: Joi.string(),
+    favorite: Joi.boolean()
 })
-    .or('name', 'email', 'phone')
+    .or('name', 'email', 'phone', 'favorite')
     .messages({
         'object.missing': 'Body must have at least one field'
     })
--- services/contactsServices.js
+++ services/contactsServices.js
@@ -1,51 +1,57 @@
-import fs from 'fs/promises';
-import path from 'path';
-import { nanoid } from "nanoid";
+// import fs from 'fs/promises';
+// import path from 'path';
+// import { nanoid } from "nanoid";
 
-const contactsPath = path.resolve("db", "contacts.json");
+// const contactsPath = path.resolve("db", "contacts.json");
 
-export async function getAllContacts() {
-    const data = await fs.readFile(contactsPath);
-    return JSON.parse(data);
-}
+// export async function getAllContacts() {
+//     const data = await fs.readFile(contactsPath);
+//     return JSON.parse(data);
+// }
 
-export async function getOneContact(contactId) {
-    const contacts = await getAllContacts();
-    const result = contacts.find(item => item.id === contactId);
+// export async function getOneContact(contactId) {
+//     const contacts = await getAllContacts();
+//     const result = contacts.find(item => item.id === contactId);
 
-    return result || null;
-}
+//     return result || null;
+// }
 
-export async function removeContact(contactId) {
-    const contacts = await getAllContacts();
-    const index = contacts.findIndex(item => item.id === contactId);
-    if (index === -1) {
-        return null;
-    }
-    const [result] = contacts.splice(index, 1);
-    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+// export async function removeContact(contactId) {
+//     const contacts = await getAllContacts();
+//     const index = contacts.findIndex(item => item.id === contactId);
+//     if (index === -1) {
+//         return null;
+//     }
+//     const [result] = contacts.splice(index, 1);
+//     await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
-    return result;
-}
+//     return result;
+// }
 
-export async function addContact(data) {
-    const contacts = await getAllContacts();
-    const newContact = {
-        id: nanoid(),
-        ...data,
-    }
-    contacts.push(newContact);
-    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-    return newContact;
-}
+// export async function addContact(data) {
+//     const contacts = await getAllContacts();
+//     const newContact = {
+//         id: nanoid(),
+//         ...data,
+//     }
+//     contacts.push(newContact);
+//     await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+//     return newContact;
+// }
 
-export async function updateContact(contactId, data) {
-    const contacts = await getAllContacts();
-    const index = contacts.findIndex(item => item.id === contactId);
-    if (index === -1) {
-        return null;
-    }
-    contacts[index] = { ...contacts[index], ...data };
-    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-    return contacts[index];
-}
+// export async function updateContact(contactId, data) {
+//     const contacts = await getAllContacts();
+//     const index = contacts.findIndex(item => item.id === contactId);
+//     if (index === -1) {
+//         return null;
+//     }
+//     contacts[index] = { ...contacts[index], ...data };
+//     await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+//     return contacts[index];
+// }
+
+import Contact from "../models/Contact.js";
+
+export const getAllContacts = () => Contact.find();
+
+export const addContact = data => Contact.create(data);
